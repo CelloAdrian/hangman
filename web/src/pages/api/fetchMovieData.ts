3cf20c27c4a37931d26e8ce30886de8c8932048b
ProjektKris <69903046+ProjektKris@users.noreply.github.com>
2022-06-28 18:41:49 +0000

    implemented http api for fetchingMovieData

--- web/src/pages/api/fetchMovieData.ts
+++ web/src/pages/api/fetchMovieData.ts
@@ -1,14 +1,28 @@
-export default async function fetchMovieData(): Promise<any> {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&page=${Math.floor(
-      Math.random() * 20 + 1
-    )}`
-  );
-  const data = await res.json();
-  const filteredData = data.results.filter(
-    (movie: { vote_count: number; }) => movie.vote_count >= 5000
-  );
-  // .replace(/\s/g, "" removes spaces
-  console.log(filteredData[0].title.toLowerCase().replace(/\s/g, ""));
-  return filteredData;
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function fetchMovieData(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET')
+    return res.status(405).json({ error: 'unsupported_method' });
+
+  try {
+    const apiResult = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${
+        process.env.TMDB_API_KEY
+      }&page=${Math.floor(Math.random() * 20 + 1)}`
+    );
+    const movieData = await apiResult.json();
+    const filteredData = movieData.results.filter(
+      (movie: { vote_count: number }) => movie.vote_count >= 5000
+    );
+    // .replace(/\s/g, "" removes spaces
+    console.log(filteredData[0].title.toLowerCase().replace(/\s/g, ''));
+
+    return res.status(200).json({ data: filteredData });
+  } catch (err) {
+    console.log((err as Error).message);
+    return res.status(500).json({ error: 'internal_server_error' });
+  }
 }
